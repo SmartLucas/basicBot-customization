c77d2e4f15dc57a347a554cf1b6513188b1dc474
SmartLucas <lucasssantos27@hotmail.com>
2014-12-09 20:57:46 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,16 +1,35 @@
 (function () {
 
+    //Define our function responsible for extending the bot.
     function extend() {
-            if (!window.bot) {
+        //If the bot hasn't been loaded properly, try again in 1 second(s).
+        if (!window.bot) {
             return setTimeout(extend, 1 * 1000);
         }
 
+        //Precaution to make sure it is assigned properly.
         var bot = window.bot;
 
+        //Load custom settings set below
         bot.retrieveSettings();
 
-        
-    
+        /*
+         Extend the bot here, either by calling another function or here directly.
+         Model code for a bot command:
+         bot.commands.commandCommand = {
+         command: 'cmd',
+         rank: 'user/bouncer/mod/manager',
+         type: 'startsWith/exact',
+         functionality: function(chat, cmd){
+         if(this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+         if( !bot.commands.executable(this.rank, chat) ) return void (0);
+         else{
+         //Commands functionality goes here.
+         }
+         }
+         }
+         */
+
         bot.commands.techCommand = {
         command: 'tech',  
         rank: 'user', 
@@ -22,20 +41,32 @@
                 API.sendChat("Tech Blog com mais regularidade para informá-lo em quais erros os desenvolvedores estão trabalhando. http://tech.plug.dj/ ");
                 }
             }
-             
-        
-                };
+        };
 
+        bot.commands.nominateCommand = {
+        command: 'nominate',  
+        rank: 'user', 
+        type: 'exact', 
+        functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+                API.sendChat("Nomeie sua comunidade aqui. http://bit.ly/1wwdOp0 ");
+                }
+            }
+        };
+
+        //Load the chat package again to account for any changes
         bot.loadChat();
-    }
 
-    
+    }
+   
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "Started",
+        botName: "SkyBot",
         language: "portuguese",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/pt.json",
-        maximumAfk: 60,
-        afkRemoval: null,
+        maximumAfk: 120,
+        afkRemoval: true,
         maximumDc: 60,
         bouncerPlus: true,
         lockdownEnabled: false,
@@ -44,44 +75,45 @@
         cycleGuard: true,
         maximumCycletime: 10,
         timeGuard: true,
-        maximumSongLength: 7,
+        maximumSongLength: 10,
         autodisable: true,
-        commandCooldown: 3,
+        commandCooldown: 30,
         usercommandsEnabled: true,
-        lockskipPosition: 1,
+        lockskipPosition: 3,
         lockskipReasons: [
-                ["tema", "A música não se encaixa nos padrões da sala. "],
-                ["op", "Essa música está na lista OP. "],
-                ["historico", "A música ainda está no histórico. "],
-                ["mix", "Você tocou um mix (muito longo) - não permitido. "],
-                ["som", "A música que você tocou tinha má qualidade ou estava sem som. "],
-                ["nsfw", "A música que você tocou é NSFW (impróprio). "],
-                ["indisponivel", "A música que você tocou está indisponível. "]
-            ],
+            ["theme", "This song does not fit the room theme. "],
+            ["op", "This song is on the OP list. "],
+            ["history", "This song is in the history. "],
+            ["mix", "You played a mix, which is against the rules. "],
+            ["sound", "The song you played had bad sound quality or no sound. "],
+            ["nsfw", "The song you contained was NSFW (image or sound). "],
+            ["unavailable", "The song you played was not available for some users. "]
+        ],
         afkpositionCheck: 15,
         afkRankCheck: "ambassador",
-        motdEnabled: null,
-        motdInterval: 8,
-        motd: "!roulette",
+        motdEnabled: false,
+        motdInterval: 5,
+        motd: "Temporary Message of the Day",
         filterChat: true,
-        etaRestriction: true,
-        welcome: false,
+        etaRestriction: false,
+        welcome: true,
         opLink: null,
         rulesLink: null,
         themeLink: null,
-        fbLink: "https://www.facebook.com/skrillex1000gr4u",
+        fbLink: null,
         youtubeLink: null,
-        website: "https://www.facebook.com/skrillex1000gr4u",
+        website: null,
         intervalMessages: [],
         messageInterval: 5,
-        songstats: false,
-        commandLiteral: "!"
+        songstats: true,
+        commandLiteral: "!",
         blacklists: {
-            NSFW: "https://rawgit.com/SmartLucas/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/SmartLucas/basicBot-customization/master/blacklists/ExampleOPlist.json"
+            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
         }
     }));
 
+    //Start the bot and extend it when it has loaded.
     $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
 
 }).call(this);
