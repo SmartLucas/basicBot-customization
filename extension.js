17592b9843abb3ce8e415a6f54cf230dc6545653
SmartLucas <lucasssantos27@hotmail.com>
2014-12-17 16:00:43 -0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -30,6 +30,19 @@
          }
          */
 
+        bot.commands.techCommand = {
+        command: 'tech',  
+        rank: 'user', 
+        type: 'exact', 
+        functionality: function (chat, cmd) {
+            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+            if (!bot.commands.executable(this.rank, chat)) return void (0);
+            else {
+                API.sendChat("Tech Blog com mais regularidade para informá-lo em quais erros os desenvolvedores estão trabalhando. http://tech.plug.dj/ ");
+                }
+            }
+        };
+
         bot.commands.baconCommand = {
             command: 'pistola',  //The command to be called. With the standard command literal this would be: !bacon
             rank: 'user', //Minimum user permission to use the command
